81dc363ef3a0ef19143709b730df31dcffbbf371
David Puleri <david@puleri.com>
2015-07-21 17:48:53 +0200

    support wisiwig

--- Admin/ContentAdmin.php
+++ Admin/ContentAdmin.php
@@ -17,7 +17,18 @@ class ContentAdmin extends Admin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('content', 'textarea', ['attr' => ['rows' => 30], 'label' => 'Contenu'])
+            ->add('content', 'sonata_formatter_type', array(
+                'event_dispatcher' => $formMapper->getFormBuilder()->getEventDispatcher(),
+                'format_field' => 'contentFormatter',
+                'source_field' => 'rawContent',
+                'source_field_options' => array(
+                    'horizontal_input_wrapper_class' => $this->getConfigurationPool()->getOption('form_type') == 'horizontal' ? 'col-lg-12' : '',
+                    'attr' => array('class' => $this->getConfigurationPool()->getOption('form_type') == 'horizontal' ? 'span10 col-sm-10 col-md-10' : '', 'rows' => 20)
+                ),
+                'ckeditor_context' => 'news',
+                'target_field' => 'content',
+                'listener' => true,
+            ))
             ->add('zone');
     }
 
